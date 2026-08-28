e7f1e77df38275dd6a19d9d4a3c2ad4d1dd357fd
typedbywill <william97623074@gmail.com>
2026-08-28 14:42:33 -0300

    fix: corrigir a chamada de fetchurl para builtins.fetchurl no script chatgpt.nix

--- pkgs/chatgpt.nix
+++ pkgs/chatgpt.nix
@@ -1,6 +1,5 @@
 { lib
 , stdenv
-, fetchurl
 , dpkg
 , buildFHSEnv
 , alsa-lib
@@ -49,9 +48,8 @@
 
 let
   version = "26.818.61809";
-  src = fetchurl {
+  src = builtins.fetchurl {
     url = "https://persistent.oaistatic.com/codex-app-prod/linux/deb/latest/chatgpt_amd64.deb";
-    hash = "sha256-Ry0D6IophX8QFbK5F12AUjoTHPG8PpAX6xqP8jTeG9o=";
   };
 
   chatgpt-unpacked = stdenv.mkDerivation {
